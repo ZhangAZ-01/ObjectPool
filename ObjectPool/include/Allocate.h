c40bcb21586e7d4c4fc1d10df4e7f505f318ed9e
zaz <869723386@qq.com>
2024-06-09 19:54:53 +0800

    first

--- ObjectPool/include/Allocate.h
+++ ObjectPool/include/Allocate.h
@@ -1,13 +1,11 @@
 #pragma once
 
 // Allocates and deallocates objects of type T.
-template<typename T>
-class Allocator
-{
+template <typename T> class Allocator {
 public:
-    Allocator() = default;
+  Allocator() = default;
 
 public:
-    virtual T * allocate() = 0;
-    virtual void deallocate(T * p) = 0;
+  virtual T *allocate() = 0;
+  virtual void deallocate(T *p) = 0;
 };
--- ObjectPool/include/ObjectPool.h
+++ ObjectPool/include/ObjectPool.h
@@ -2,28 +2,21 @@
 
 #include <stdio.h>
 
-template<typename T, typename Allocator>
-class ObjectPool
-{
+template <typename T, typename Allocator> class ObjectPool {
 private:
-    Allocator allocator_;
-    
-public:
-    ObjectPool() = default;
-    ~ObjectPool() = default;
+  Allocator allocator_;
 
 public:
-    void * allocate(size_t n)
-    {
-        if (sizeof(T) != n)
-        {
-            printf("Error");
-            return nullptr;
-        }
-        return allocator_.allocate();
+  ObjectPool() = default;
+  ~ObjectPool() = default;
+
+public:
+  void *allocate(size_t n) {
+    if (sizeof(T) != n) {
+      printf("Error");
+      return nullptr;
     }
-    void deallocate(void * p)
-    {
-        allocator_.deallocate(static_cast<T *>(p));
-    }
+    return allocator_.allocate();
+  }
+  void deallocate(void *p) { allocator_.deallocate(static_cast<T *>(p)); }
 };
